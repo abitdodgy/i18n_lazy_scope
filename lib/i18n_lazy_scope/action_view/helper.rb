f1e6d872436f7bec9ee2dcfbe8b7354749b8adf6
Mohamad El-Husseini <husseini.mel@gmail.com>
2015-01-24 09:49:42 -0200

    scope key

--- lib/i18n_lazy_scope/action_view/helper.rb
+++ lib/i18n_lazy_scope/action_view/helper.rb
@@ -1,8 +1,15 @@
 module I18nLazyScope::ActionView
   module Helper
     def lazy_scope
-      raise @virtual_path.to_s
-      [:views, controller_name, action_name]
+      [:views, controller_name, scope_key_by_partial(:hello)]
+    end
+
+    def scope_key_by_partial(key)
+      if @virtual_path
+        @virtual_path.gsub(%r{/_?}, ".") + key.to_s
+      else
+        raise "Cannot use t(#{key.inspect}) shortcut because path is not available"
+      end
     end
   end
 end
