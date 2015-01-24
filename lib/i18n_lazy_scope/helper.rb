3177077985395699c2d1ff82120830ca2e15f186
Mohamad El-Husseini <husseini.mel@gmail.com>
2015-01-24 08:13:46 -0200

    fix extend error

--- lib/i18n_lazy_scope/helper.rb
+++ lib/i18n_lazy_scope/helper.rb
@@ -1,13 +1,5 @@
-require "i18n_lazy_scope/action_controller/helper"
-require "i18n_lazy_scope/action_mailer/helper"
-require "i18n_lazy_scope/action_view/helper"
-
 module I18nLazyScope
   module Helper
-    include I18nLazyScope::ActionController::Helper
-    include I18nLazyScope::ActionMailer::Helper
-    include I18nLazyScope::ActionView::Helper
-
     def t_scoped(key, **args)
       t(key, scope: lazy_scope, **args)
     end
--- lib/i18n_lazy_scope/railtie.rb
+++ lib/i18n_lazy_scope/railtie.rb
@@ -1,18 +1,26 @@
 require "rails"
+
+require "i18n_lazy_scope/action_controller/helper"
+require "i18n_lazy_scope/action_mailer/helper"
+require "i18n_lazy_scope/action_view/helper"
+
 require "i18n_lazy_scope/helper"
 
 module I18nLazyScope
   class Railtie < Rails::Railtie
     initializer "i18n_lazy_scope.helpers" do
       ActiveSupport.on_load(:action_view) do
+        include I18nLazyScope::ActionView::Helper
         include I18nLazyScope::Helper
       end
 
       ActiveSupport.on_load(:action_controller) do
+        include I18nLazyScope::ActionController::Helper
         include I18nLazyScope::Helper
       end
 
       ActiveSupport.on_load(:action_mailer) do
+        include I18nLazyScope::ActionMailer::Helper
         include I18nLazyScope::Helper
       end
     end
